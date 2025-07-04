dca6cdb5dd442581d83f678ee3047a58d9a4170c
Mit S. <121121987+2004Luci@users.noreply.github.com>
2025-07-04 11:39:37 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,11 +7,6 @@ const Footer = () => {
                 <p className='text-white-500'>© 2025 Mit. All rights reserved.</p>
             </div>
             <div className='flex gap-3'>
-                <a href="https://github.com/2004Luci/threejs_portfolio" target='_blank' rel='noreferrer'>
-                    <div className='sm:social-icon social-icon_small'>
-                        <img src="/assets/github.svg" alt="github" className='w-1/2 h-1/2' />
-                    </div>
-                </a>
                 <a href="https://www.linkedin.com/in/mit-sheth-250b71351/" target='_blank' rel='noreferrer'>
                     <div className='sm:social-icon social-icon_small'>
                         <img src="/assets/linkedin.svg" alt="linkedin" className='w-1/2 h-1/2 invert' />
